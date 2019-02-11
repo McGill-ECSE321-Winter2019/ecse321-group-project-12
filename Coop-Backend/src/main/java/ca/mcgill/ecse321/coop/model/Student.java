edd20ffa810dbb3f98f69838a0fa6b4b9998ec80
MA9521 <46541787+MA9521@users.noreply.github.com>
2019-02-10 21:44:11 -0500

    Update Student.java

--- Coop-Backend/src/main/java/ca/mcgill/ecse321/coop/model/Student.java
+++ Coop-Backend/src/main/java/ca/mcgill/ecse321/coop/model/Student.java
@@ -1,184 +1,44 @@
 package ca.mcgill.ecse321.coop.model;
 
+import ca.mcgill.ecse321.coop.model.User;
+
 import javax.persistence.Entity;
-import java.sql.Date;
-import javax.persistence.ManyToOne;
-import javax.persistence.Id;
-import javax.persistence.CascadeType;
-import javax.persistence.OneToOne;
 import java.util.Set;
 import javax.persistence.OneToMany;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToOne;
 
 @Entity
-public class CoOpJob {
-	private Student student;
+public class Student extends User {
+	private Set<CoOpJob> coOpJob;
 
-	@ManyToOne(optional = false)
-	public Student getStudent() {
-		return this.student;
+	@OneToMany(mappedBy = "student")
+	public Set<CoOpJob> getCoOpJob() {
+		return this.coOpJob;
 	}
 
-	public void setStudent(Student student) {
-		this.student = student;
+	public void setCoOpJob(Set<CoOpJob> coOpJobs) {
+		this.coOpJob = coOpJobs;
 	}
 
-	private Date startDate;
+	private boolean allowCV = false;
 
-	private void setStartDate(Date value) {
-		this.startDate = value;
+	public void setAllowCV(boolean value) {
+		this.allowCV = value;
 	}
 
-	private Date getStartDate() {
-		return this.startDate;
+	public boolean isAllowCV() {
+		return this.allowCV;
 	}
 
-	private Date endDate;
+	private boolean allowTranscript = false;
 
-	private void setEndDate(Date value) {
-		this.endDate = value;
+	public void setAllowTranscript(boolean value) {
+		this.allowTranscript = value;
 	}
 
-	private Date getEndDate() {
-		return this.endDate;
-	}
-
-	private String name;
-
-	public void setName(String value) {
-		this.name = value;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	private boolean studentHasStarted;
-
-	public void setStudentHasStarted(boolean value) {
-		this.studentHasStarted = value;
-	}
-
-	public boolean isStudentHasStarted() {
-		return this.studentHasStarted;
-	}
-
-	private boolean studentHasEnded;
-
-	public void setStudentHasEnded(boolean value) {
-		this.studentHasEnded = value;
-	}
-
-	public boolean isStudentHasEnded() {
-		return this.studentHasEnded;
-	}
-
-	private Employer employer;
-
-	@ManyToOne(optional = false)
-	public Employer getEmployer() {
-		return this.employer;
-	}
-
-	public void setEmployer(Employer employer) {
-		this.employer = employer;
-	}
-
-	private boolean studentOfferedAJobLater;
-
-	public void setStudentOfferedAJobLater(boolean value) {
-		this.studentOfferedAJobLater = value;
-	}
-
-	public boolean isStudentOfferedAJobLater() {
-		return this.studentOfferedAJobLater;
-	}
-
-	private String jobId;
-
-	public void setJobId(String value) {
-		this.jobId = value;
-	}
-
-	@Id
-	public String getJobId() {
-		return this.jobId;
-	}
-
-	private Contract contract;
-
-	@OneToOne(mappedBy = "coOpJob", cascade = { CascadeType.ALL }, optional = false)
-	public Contract getContract() {
-		return this.contract;
-	}
-
-	public void setContract(Contract contract) {
-		this.contract = contract;
-	}
-
-	private Set<ProgressReport> progressReports;
-
-	@OneToMany(mappedBy = "coOpJob", cascade = { CascadeType.ALL })
-	public Set<ProgressReport> getProgressReports() {
-		return this.progressReports;
-	}
-
-	public void setProgressReports(Set<ProgressReport> progressReportss) {
-		this.progressReports = progressReportss;
-	}
-
-	private StudentEvaluationByEmployer studentEvaluationByEmployer;
-
-	@OneToOne(mappedBy = "coOpJob", cascade = { CascadeType.ALL }, optional = false)
-	public StudentEvaluationByEmployer getStudentEvaluationByEmployer() {
-		return this.studentEvaluationByEmployer;
-	}
-
-	public void setStudentEvaluationByEmployer(StudentEvaluationByEmployer studentEvaluationByEmployer) {
-		this.studentEvaluationByEmployer = studentEvaluationByEmployer;
-	}
-
-	private TaxCreditForm taxCreditForm;
-
-	@OneToOne(mappedBy = "coOpJob", cascade = { CascadeType.ALL }, optional = false)
-	public TaxCreditForm getTaxCreditForm() {
-		return this.taxCreditForm;
-	}
-
-	public void setTaxCreditForm(TaxCreditForm taxCreditForm) {
-		this.taxCreditForm = taxCreditForm;
-	}
-
-	private ProofOfPlacement proofOfPlacement;
-
-	@OneToOne(mappedBy = "coOpJob", cascade = { CascadeType.ALL }, optional = false)
-	public ProofOfPlacement getProofOfPlacement() {
-		return this.proofOfPlacement;
-	}
-
-	public void setProofOfPlacement(ProofOfPlacement proofOfPlacement) {
-		this.proofOfPlacement = proofOfPlacement;
-	}
-
-	private JobDetail jobDetail;
-
-	@OneToOne(mappedBy = "coOpJob", cascade = { CascadeType.ALL }, optional = false)
-	public JobDetail getJobDetail() {
-		return this.jobDetail;
-	}
-
-	public void setJobDetail(JobDetail jobDetail) {
-		this.jobDetail = jobDetail;
-	}
-
-	private Set<Other> others;
-
-	@OneToMany(mappedBy = "coOpJob", cascade = { CascadeType.ALL })
-	public Set<Other> getOthers() {
-		return this.others;
-	}
-
-	public void setOthers(Set<Other> otherss) {
-		this.others = otherss;
+	public boolean isAllowTranscript() {
+		return this.allowTranscript;
 	}
 
 }
