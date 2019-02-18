a2489b2c4508d852c852c57e70b7e5be5c2a417e
MA9521 <mostafa.abdelwahab@mail.mcgill.ca>
2019-02-17 19:42:43 -0500

    added model image and explanations

--- MyCo-Op/src/ca/mcgill/ecse321/coop/model/Administrator.java
+++
@@ -1,7 +0,0 @@
-package ca.mcgill.ecse321.coop.model;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Administrator extends User {
-}
--- MyCo-Op/src/ca/mcgill/ecse321/coop/model/CV.java
+++
@@ -1,9 +0,0 @@
-package ca.mcgill.ecse321.coop.model;
-
-import javax.persistence.Entity;
-import javax.persistence.OneToOne;
-
-@Entity
-public class CV extends Document {
-
-}
