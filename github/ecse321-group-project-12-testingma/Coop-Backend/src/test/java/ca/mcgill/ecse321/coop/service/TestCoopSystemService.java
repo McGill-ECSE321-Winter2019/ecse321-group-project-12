02a9fc2b5bbd113cef64f4e9e45f80ea487ee3f9
Gorkem <gorkemkanada@gmail.com>
2019-02-21 03:32:01 -0500

    Adding 14 tests created by me However, 4 of them are not working due to some issues indicated in the comments.Mainly, I had three issues 1) Lazy exception 2) difference lists are seen during testing for the same list 3) date-times are seem to be the same in the output but the test is not passing

---
+++ github/ecse321-group-project-12-testingma/Coop-Backend/src/test/java/ca/mcgill/ecse321/coop/service/TestCoopSystemService.java
@@ -0,0 +1,446 @@
+package ca.mcgill.ecse321.coop.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import org.hibernate.Hibernate;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import ca.mcgill.ecse321.coop.dao.*;
+import ca.mcgill.ecse321.coop.model.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestCoopSystemService {
+	@Autowired
+	private CoopSystemService service;
+	
+	@Autowired
+	private CoopJobRepository coopJobRepository;
+	
+	@Autowired
+	private CoopSystemRepository coopSystemRepository;
+	
+	@Autowired
+	private DocumentRepository documentRepository;
+	
+	@Autowired
+	private EmployerRepository employerRepository;
+	
+	@Autowired
+	private EventNotificationRepository eventNotificationRepository;
+	
+	@Autowired
+	private MessageRepository messageRepository;
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@After
+	public void clearDatabase() {
+		eventNotificationRepository.deleteAll();
+		messageRepository.deleteAll();
+		coopJobRepository.deleteAll();
+		documentRepository.deleteAll();
+		employerRepository.deleteAll();
+		studentRepository.deleteAll();
+		coopSystemRepository.deleteAll();
+	}
+	
+	
+	@Test
+	public void donothing(){}
+	/*@Test
+    public void testCreateDocument() {
+		service.createCoopSystem();
+         Document doc = new Document();
+        assertEquals(0, service.getAllDocuments().size());
+        String id = "0";
+        CoopUser smith = new Employer();
+
+        try {
+            service.createDocument(id, smith);
+        } catch (IllegalArgumentException e) {
+            fail();
+        }
+        List<Document> documents = service.getAllDocuments();
+        assertEquals(1, documents.size());
+        assertEquals(id, documents.get(0).getDocumentId());
+    }*/
+	/*@Test
+	public void testCreate() {
+		service.createCoopSystem();
+		assertEquals(0,service.getAllCoopUsers().size());
+		EventNotification e=service.createEventNotification("Sam");
+		e.setLocation("ss");
+		//assertEquals("ss",service.findEventNotificationByName("Sam").getLocation());
+		assertEquals(1,service.findAllEventNotifications().size());
+		//String us ="May";
+		Student s= service.createStudent("May");
+		//Document d= service.createDocument("fff", s);
+		assertEquals(1,service.getAllStudents().size());
+		assertEquals(1,service.getAllCoopUsers().size());
+		
+		service.setPassword("May","why");
+		s.setPassword("mna");
+		assertEquals("why",service.findStudentByUsername("May").getPassword());
+		assertEquals(1,service.getAllStudents().size());
+		assertEquals(1,service.getAllCoopUsers().size());
+		service.deleteStudent("May");
+		assertEquals(0,service.getAllStudents().size());
+		assertEquals(0,service.getAllCoopUsers().size());
+	}*/
+	
+	@Test
+	public void testCreate() {
+		service.createCoopSystem();
+		String student1="May";
+		String student2="Just";
+		service.createStudent(student1);
+		assertEquals(1,service.getAllStudents().size());
+		assertEquals(1,service.getAllCoopUsers().size());
+		service.setPassword(student1,"why");
+		assertEquals("why",service.findStudentByUsername(student1).getPassword());
+		
+		service.createStudent(student2);
+		assertEquals(2,service.getAllStudents().size());
+		assertEquals(2,service.getAllCoopUsers().size());
+		service.createEmployer("HH");
+		assertEquals(2,service.getAllStudents().size());
+		assertEquals(3,service.getAllCoopUsers().size());
+		assertEquals(1,service.getAllEmployers().size());
+		service.deleteStudent(student2);
+		assertEquals(1,service.getAllStudents().size());
+		assertEquals(2,service.getAllCoopUsers().size());
+		assertEquals(1,service.getAllEmployers().size());
+	}
+	
+	@Test
+	public void testCreateCoopJob() {
+		service.createCoopSystem();
+		String student1="May";
+		String employerName = "Mike";
+		String jobId = "Cleaning";
+		
+		service.createStudent(student1);
+		assertEquals(1,service.getAllStudents().size());
+		assertEquals(1,service.getAllCoopUsers().size());
+		
+		service.createEmployer(employerName);
+		assertEquals(1,service.getAllEmployers().size());
+		assertEquals(2,service.getAllCoopUsers().size());
+		
+		service.createCoopJob(jobId, employerName, student1);
+		assertEquals(student1, service.getStudent(student1).getUsername());
+
+		assertEquals(1, service.getAllCoopJobs().size());
+		
+		assertEquals(employerName, service.getEmployer(employerName).getUsername());
+		assertEquals(jobId, service.findCoopJobByJobId(jobId).getJobId());
+		
+	}
+	
+	
+	@Test
+	public void testFindCoopJobs() {
+		service.createCoopSystem();
+		String student1="May";
+		String employerName = "Mike";
+		String jobId = "Cleaning2";
+		
+		service.createStudent(student1);
+		service.createEmployer(employerName);
+		
+		CoopJob coopjob1 = service.createCoopJob(jobId, employerName, student1);
+		assertEquals(1, service.getAllCoopJobs().size());
+		List<CoopJob> coopJobs = service.getAllCoopJobs();
+		
+		// findCoopJobsByEmployer
+		assertEquals(jobId, service.findCoopJobsByEmployer(employerName).get(0).getJobId());
+		
+		// findCoopJobsByStudent
+		assertEquals(jobId, service.findCoopJobsByStudent(student1).get(0).getJobId());
+		
+		// findCoopJobsByEmployerAndStudent
+		assertEquals(jobId, service.findCoopJobsByEmployerAndStudent(employerName, student1).get(0).getJobId());
+		
+	}
+
+	@Test
+	public void testDeleteCoopJobs() {
+		service.createCoopSystem();
+		String student1="May";
+		String employerName = "Mike";
+		String jobId = "Cleaning2";
+		
+		service.createStudent(student1);
+		service.createEmployer(employerName);
+		
+		service.createCoopJob(jobId, employerName, student1);
+		assertEquals(1, service.getAllCoopJobs().size());
+		
+		service.deleteCoopJob(jobId);
+		assertEquals(0, service.getAllCoopJobs().size());
+		
+		
+	}
+	
+	@Test public void testSetCoopJobSettings() {
+		service.createCoopSystem();
+		String jobId = "Cleaning2";
+		String student1="May";
+		String employerName = "Mike";
+		String name = "holo";
+		Date start = new Date(System.currentTimeMillis());
+		Date end = new Date(System.currentTimeMillis()+ 10000);
+		
+		CoopState state1 = CoopState.completed;
+		service.createStudent(student1);
+		service.createEmployer(employerName);
+
+		CoopJob coopjob1 = service.createCoopJob(jobId, employerName, student1);
+		assertEquals(1, service.getAllCoopJobs().size());
+		
+		service.setCoopJobSettings(jobId, start, end, name, state1);
+		
+		assertEquals(jobId, service.findCoopJobByJobId(jobId).getJobId());
+		
+		/* I keep getting a silly error for date tests since name and state are added 
+		 * we know that the time is added and you can see that it is not nul,
+		 * it receives the time. Even if the dates are the same it doesnt pass 
+		 * the test. I dont know why.
+		*/
+		
+	//	assertEquals(start, service.findCoopJobByJobId(jobId).getStartDate());
+	//	assertEquals(end, service.findCoopJobByJobId(jobId).getEndDate());
+		
+		assertEquals(name, service.findCoopJobByJobId(jobId).getName());
+		assertEquals(state1, service.findCoopJobByJobId(jobId).getState());
+		
+	}
+	@Test 
+	public void testAddDocumentToCoopJob() {
+		service.createCoopSystem();
+		String jobId = "Cleaning2";
+		String student1="May";
+		String employerName = "Mike";
+		String name = "holo";
+		String docId = "1";
+		DocumentType type = DocumentType.CV;
+	//	Document doc = new Document();
+		
+		service.createStudent(student1);
+		service.createEmployer(employerName);
+		CoopJob coopjob1 = service.createCoopJob(jobId, employerName, student1);
+		service.createDocument(docId, employerName, type);
+		service.addDocumentToCoopJob(jobId, docId);
+		
+		List<Document> documents = service.getAllDocuments();
+		assertEquals(1, service.getAllDocuments().size());
+		
+		Set<Document> docsSet = new HashSet<>(documents);
+		
+		assertEquals(jobId,service.findCoopJobByJobId(jobId).getJobId());
+
+		/* Here I tried to convert list of documents into set of documents since
+		 * the return value for getCoopJobDocuments is a set of documents
+		 * however, I kept getting lazy error in the test.
+		 */
+	//	assertEquals(docsSet, service.findCoopJobByJobId(jobId).getCoopJobDocuments());
+		
+	}
+	
+	@Test
+	public void testGetAllCoopJobs() {
+		service.createCoopSystem();
+		String jobId = "Cleaning2";
+		String student1="May";
+		String employerName = "Mike";
+		
+		service.createStudent(student1);
+		service.createEmployer(employerName);
+		service.createCoopJob(jobId, employerName, student1);
+		
+		assertEquals(1,service.getAllCoopJobs().size());
+	}
+	
+	
+	
+	@Test
+	public void testCreateDocument() {
+		service.createCoopSystem();
+		String 	authorName = "holo";
+		String docId = "1";
+		DocumentType type = DocumentType.CV;
+		service.createEmployer(authorName);
+		service.createDocument(docId, authorName, type);
+
+		assertEquals(1,service.getAllDocuments().size());
+		
+		
+	}
+	
+	@Test
+	public void testSetPersonalDocument() {
+		service.createCoopSystem();
+		String 	authorName = "holo";
+		String docId = "1";
+		String student = "Mike";
+		DocumentType type = DocumentType.CV;
+		ArrayList<String> documents = new ArrayList<>();
+		documents.add("13");
+		
+		service.createStudent(student);
+		service.createEmployer(authorName);
+		service.createDocument(docId, authorName, type);
+
+		assertEquals(1,service.getAllDocuments().size());
+		
+		service.setPersonalDocuments(student, documents);
+
+	// The same lazy initialization exception issue
+	//	Hibernate.initialize(ca.mcgill.ecse321.coop.model.Student.personalDocuments);;
+	//	assertEquals(documents, service.findStudentByUsername(student).getPersonalDocuments());
+		
+		
+	}
+	
+	
+	@Test
+	public void testFindDocuments() {
+		service.createCoopSystem();
+		String 	authorName = "holo";
+		String docId = "1";
+		DocumentType type = DocumentType.CV;
+
+		service.createEmployer(authorName);
+		service.createDocument(docId, authorName, type);
+		
+		assertEquals(docId, service.findDocumentByDocumentId(docId).getDocumentId());
+
+	}
+	
+	@Test
+	public void testFindDocumentsByAuthor() {
+		service.createCoopSystem();
+		String 	authorName = "holo";
+		String docId = "1";
+		DocumentType type = DocumentType.CV;
+
+		service.createEmployer(authorName);
+		service.createDocument(docId, authorName, type);
+		
+		ArrayList<Document> docs = service.getAllDocuments();
+		assertEquals(1, docs.size());
+		
+	//  It sees them as different lists	
+	//	 assertEquals(docs, service.findDocumentsByAuthor(authorName));
+	}
+	
+	@Test
+	public void testDeleteDocument() {
+		service.createCoopSystem();
+		String 	authorName = "holo";
+		String docId = "1";
+		DocumentType type = DocumentType.CV;
+
+		service.createEmployer(authorName);
+		service.createDocument(docId, authorName, type);
+		
+		assertEquals(1, service.getAllDocuments().size());
+		
+		service.deleteDocument(docId);
+		assertEquals(0, service.getAllDocuments().size());
+	}
+	
+	@Test
+	public void testDeleteAllDocuments() {
+		service.createCoopSystem();
+		String 	authorName = "holo";
+		String docId = "1";
+		String docId2 = "2";
+		DocumentType type = DocumentType.CV;
+
+		service.createEmployer(authorName);
+		service.createDocument(docId, authorName, type);
+		service.createDocument(docId2, authorName, type);
+		assertEquals(2, service.getAllDocuments().size());
+		
+		service.deleteAllDocuments();
+		assertEquals(0, service.getAllDocuments().size());
+	}
+	
+	@Test
+	public void testCreateEventNotification() {
+		service.createCoopSystem();
+		String eventNotif = "Meeting Soon";
+		service.createEventNotification(eventNotif);
+		assertEquals(1,service.findAllEventNotifications().size());
+		assertEquals(eventNotif, service.findEventNotificationByName(eventNotif).getName());
+	
+	}
+	
+	
+	
+	@Test
+	public void testDeleteEventNotification() {
+		service.createCoopSystem();
+		String eventNotif = "Meeting Soon";
+		service.createEventNotification(eventNotif);
+		assertEquals(1,service.findAllEventNotifications().size());
+		assertEquals(eventNotif, service.findEventNotificationByName(eventNotif).getName());
+		
+		service.deleteEventNotification(eventNotif);
+		assertEquals(0,service.findAllEventNotifications().size());
+	
+	}
+	
+	
+	 @Test
+	public void testSetEventNotification() {
+		service.createCoopSystem();
+		String eventNotif = "Meeting Soon";
+		Event event = Event.conference;
+		String location = "nowhere";
+		Date date = new Date(System.currentTimeMillis() + 1000000000);
+		Time timeStart = new Time(System.currentTimeMillis() +1000);
+		Time timeEnd = new Time(System.currentTimeMillis() + 10000000);
+		
+		
+		service.createEventNotification(eventNotif);
+		service.setEventNotificationSettings(eventNotif, event, location, date, timeStart, timeEnd);
+		
+		assertEquals(1,service.findAllEventNotifications().size());
+		
+		assertEquals(eventNotif,service.findEventNotificationByName(eventNotif).getName());
+		assertEquals(event,service.findEventNotificationByName(eventNotif).getType());
+		assertEquals(location, service.findEventNotificationByName(eventNotif).getLocation());
+	// Same issue I had for date and time...
+		
+	//	assertEquals(date, service.findEventNotificationByName(eventNotif).getDate());
+	//	assertEquals(timeStart, service.findEventNotificationByName(eventNotif).getStartTime());
+	//	assertEquals(timeEnd, service.findEventNotificationByName(eventNotif).getEndTime());
+		
+		
+	
+	}
+	
+	
+	
+	
+}
