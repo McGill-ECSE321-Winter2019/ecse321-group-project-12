2ef77ff2002feefc5e9c8006e6ec783a8eb53e37
SameenMahtab <46800007+SameenMahtab@users.noreply.github.com>
2019-03-04 16:36:14 -0500

    Add files via upload
    integratedTest

---
+++ Coop-Backend/src/test/java/ca/mcgill/ecse321/coop/interfaceTest.java
@@ -0,0 +1,180 @@
+package ca.mcgill.ecse321.coop;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.junit.*;
+import org.junit.jupiter.api.Test;
+
+class interfaceTest {
+	
+	@Test
+	void testCreateSystem() {
+		testCreate("https://ecse321-group12.herokuapp.com/coopsystem");
+	}
+	
+	@Test
+	void testCreateStudent() {
+		testCreate("https://ecse321-group12.herokuapp.com/students/Mahtab");
+		testDelete("https://ecse321-group12.herokuapp.com/students/Mahtab");
+	}
+	@Test
+	void testCreateEmployer() {
+		testCreate("https://ecse321-group12.herokuapp.com/employers/Brown");
+		testDelete("https://ecse321-group12.herokuapp.com/employers/Brown");
+	}
+	@Test
+	void testSetPassword() {
+		testCreate("https://ecse321-group12.herokuapp.com/students/Richard");
+		testCreate("https://ecse321-group12.herokuapp.com/setPassword?Username=Richard&Password=1234");
+		queryService("https://ecse321-group12.herokuapp.com/students/Richard");
+		testDelete("https://ecse321-group12.herokuapp.com/students/Richard");
+	}	
+	@Test
+	void createDocument() {
+		testCreate("https://ecse321-group12.herokuapp.com/createDocument?UserName=Bloch&DocumentId=moon&Type=CV");
+		testDelete("https://ecse321-group12.herokuapp.com/students/Bloch");
+	}	
+	@Test
+	void testSetPreferencesStudentPreferences() {
+		testCreate("https://ecse321-group12.herokuapp.com/students/John1");
+		testCreate("https://ecse321-group12.herokuapp.com/studentPreferences?StudentName=John1&AllowCV=false&AllowTranscript=true");
+        testDelete("https://ecse321-group12.herokuapp.com/students/John1");
+	}
+	@Test
+	void testSetPreferencesStudentPersonal() {
+		testCreate("https://ecse321-group12.herokuapp.com/students/Johnny1");
+		testCreate("https://ecse321-group12.herokuapp.com/studentPersonal?StudentName=Johnny1&PersonalDocumentsIds=moon");
+        testDelete("https://ecse321-group12.herokuapp.com/students/Johnny1");
+	}	
+	
+	/*@Test
+	void testCreateAndSendMessage() {
+		testCreate("https://ecse321-group12.herokuapp.com/students/John2");
+		//testCreate("https://ecse321-group12.herokuapp.com/students/Johnny2");
+		testCreate("https://ecse321-group12.herokuapp.com/newMessage?MessageId=Aga1&SenderName=John2&ReceiverName=Johnny2&Content=Aga&ListofAttachementsIds=[moon]");
+	    testDelete("https://ecse321-group12.herokuapp.com/students/John2");
+	    //testDelete("https://ecse321-group12.herokuapp.com/students/Johnny2");
+	    testDelete("https://ecse321-group12.herokuapp.com/newMessage?MessageId=Aga1");
+	}*/
+	
+	@Test
+	void testDeleteSystem() {	
+		testDelete("https://ecse321-group12.herokuapp.com/coopsystem");
+	}
+	@Test
+	void testCreateEventNotification() {
+
+		testCreate("https://ecse321-group12.herokuapp.com/createEventNotification?EventNotificationId=HelloWorld");
+	
+	}	
+	@Test
+	void setEventNotificationSettings() {
+		
+		testCreate("https://ecse321-group12.herokuapp.com/setEventSettings?EventNotificationId=HelloWorld&Type=conferece&Location=Amsterdam&Date=2019-02-21&StartTime=13:10:00&EndTime=18:30:00");
+	
+	}	
+	//send url of query
+	void queryService(String s) {	
+    			
+		testGet(s);
+	}
+
+
+	void testDelete(String url) {
+		
+    try {		
+	   		URL targetUrl = new URL(url);			 
+            HttpURLConnection httpConnection = (HttpURLConnection) targetUrl.openConnection();          
+            httpConnection.setDoOutput(true);
+            httpConnection.setRequestMethod("DELETE");
+            httpConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+ 
+            if (httpConnection.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                    + httpConnection.getResponseCode());
+            }
+
+            //System.out.println(httpConnection.getResponseCode());
+            httpConnection.disconnect();            
+          } catch (MalformedURLException e) {
+            e.printStackTrace();
+          } catch (IOException e) {
+            e.printStackTrace();            
+          }		
+	}
+
+	void testCreate(String url) {
+		
+		try {		
+			
+	   		URL targetUrl = new URL(url);			 
+            HttpURLConnection httpConnection = (HttpURLConnection) targetUrl.openConnection();          
+            httpConnection.setDoOutput(true);
+            httpConnection.setRequestMethod("POST");
+            httpConnection.setRequestProperty("Content-Type", "application/json");
+ 
+            if (httpConnection.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                    + httpConnection.getResponseCode());
+            }
+            
+            InputStreamReader input = new InputStreamReader((httpConnection.getInputStream()));	 
+            BufferedReader responseBuffer = new BufferedReader(input);
+ 
+            if(url.contentEquals("https://ecse321-group12.herokuapp.com/coopsystem")) {
+            	
+            }else {
+            	String output;
+            System.out.println("Output from Server:\n");
+            while ((output = responseBuffer.readLine()) != null) {
+                System.out.println(output);
+            }
+            }
+            input.close();
+            httpConnection.disconnect();            
+          } catch (MalformedURLException e) { 
+            e.printStackTrace();
+          } catch (IOException e) {
+            e.printStackTrace();         
+          }		
+	}
+	
+
+	void testGet(String url) {
+		try {
+			 
+            URL restServiceURL = new URL(url);
+ 
+            HttpURLConnection httpConnection = (HttpURLConnection) restServiceURL.openConnection();
+            httpConnection.setRequestMethod("GET");
+            httpConnection.setRequestProperty("Accept", "application/json");
+ 
+            if (httpConnection.getResponseCode() != 200) {
+                throw new RuntimeException("HTTP GET Request Failed with Error code : "
+                        + httpConnection.getResponseCode());
+            }
+ 
+            BufferedReader responseBuffer = new BufferedReader(new InputStreamReader(
+                (httpConnection.getInputStream())));
+ 
+            String output;
+            System.out.println("Output from Server:  \n");
+ 
+            while ((output = responseBuffer.readLine()) != null) {
+                System.out.println(output);
+            }
+            httpConnection.disconnect();
+          } catch (MalformedURLException e) {
+            e.printStackTrace();
+          } catch (IOException e) {
+            e.printStackTrace();
+          }      
+	}
+
+}
